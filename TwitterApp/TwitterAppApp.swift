3bce9195a7bc9bad008324dbb9cbc6b839683d98
kyohei <takanohashi.com@gmail.com>
2023-09-13 14:42:59 +0900

    Initial Commit

---
+++ TwitterApp/TwitterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterAppApp.swift
+//  TwitterApp
+//
+//  Created by 鷹觜恭平 on 2023/09/13.
+//
+
+import SwiftUI
+
+@main
+struct TwitterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
